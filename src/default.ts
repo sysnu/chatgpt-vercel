56eca94e96de90edb9a1dab46b2277419152210c
sysnu <70570959+sysnu@users.noreply.github.com>
2023-03-13 16:05:06 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -6,4 +6,7 @@ export const defaultSetting = {
   systemRule: ""
 }
 
-export const defaultMessage = ``
+export const defaultMessage = `
+- 由 OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 由 [@ourongxing](https://github.com/ourongxing) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
+`
